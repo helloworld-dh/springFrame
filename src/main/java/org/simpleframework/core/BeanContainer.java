d4f792304d1c5771e0dada3f7ecb5adee87d1c48
Han <duhanx@163.com>
2022-06-18 23:40:24 +0800

    bean 容器

--- src/main/java/org/simpleframework/core/BeanContainer.java
+++ src/main/java/org/simpleframework/core/BeanContainer.java
@@ -1,4 +1,11 @@
-package org.simpleframework.core.annotation;
+package org.simpleframework.core;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @ClassName: BeanContainer
@@ -6,6 +13,26 @@ package org.simpleframework.core.annotation;
  * @Author: Du
  * @Date: 2022/6/18
  */
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BeanContainer {
-    
+
+    /**
+     * 存放所有被配置标记的目标对象的map
+     */
+    private final Map<Class<?>,Object> beanMap = new ConcurrentHashMap<>();
+    /**
+     * 获取Bean容器的实例
+     * @return
+     */
+    public static BeanContainer getInstance(){
+        return ContainerHolder.HOLDER.instance;
+    }
+    private enum ContainerHolder{
+        HOLDER;
+        private BeanContainer instance;
+        ContainerHolder(){
+            instance = new BeanContainer();
+        }
+    }
 }
