32cd341f0cf79945dfe0a0b178c8f8687db57dff
Han <duhanx@163.com>
2022-06-21 10:01:15 +0800

    jdkproxy

--- src/main/java/demo/pattern/proxy/jdkproxy/AlipayInvocationHandler.java
+++ src/main/java/demo/pattern/proxy/jdkproxy/AlipayInvocationHandler.java
@@ -1,8 +1,35 @@
-package demo.pattern.proxy.jdkproxy;/**
-*@ClassName: AlipayInvocationHandler
-*@Description: 
-*@Author: Du
-*@Date: 2022/6/21
-*/
-public class AlipayInvocationHandler {
+package demo.pattern.proxy.jdkproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @ClassName: AlipayInvocationHandler
+ * @Description:
+ * @Author: Du
+ * @Date: 2022/6/21
+ */
+public class AlipayInvocationHandler implements InvocationHandler {
+
+    private Object targetObject;
+
+    public AlipayInvocationHandler(Object targetObject){
+        this.targetObject = targetObject;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        beforePay();
+        Object result = method.invoke(targetObject, args);
+        afterPay();
+        return result;
+    }
+
+    private void afterPay() {
+        System.out.println("支付给店家");
+    }
+
+    private void beforePay() {
+        System.out.println("从招行取款");
+    }
 }
--- src/main/java/demo/pattern/proxy/jdkproxy/JdkDynamicProxyUtil.java
+++ src/main/java/demo/pattern/proxy/jdkproxy/JdkDynamicProxyUtil.java
@@ -1,8 +1,18 @@
-package demo.pattern.proxy.jdkproxy;/**
-*@ClassName: JdkDynamicProxyUtil
-*@Description: 
-*@Author: Du
-*@Date: 2022/6/21
-*/
+package demo.pattern.proxy.jdkproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * @ClassName: JdkDynamicProxyUtil
+ * @Description:
+ * @Author: Du
+ * @Date: 2022/6/21
+ */
 public class JdkDynamicProxyUtil {
+    public static <T> T newProxyInstance(T targetObject, InvocationHandler handler){
+        ClassLoader classLoader = targetObject.getClass().getClassLoader();
+        Class<?>[] interfaces = targetObject.getClass().getInterfaces();
+        return (T) Proxy.newProxyInstance(classLoader,interfaces,handler);
+    }
 }
